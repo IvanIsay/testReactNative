cdf1d4393befd144f74e7efa21934cf1dbfe0575
lOkY <102552151+IvanIsay@users.noreply.github.com>
2024-04-25 20:52:23 -0600

    funcionesJS

---
+++ contexto/index.js
@@ -0,0 +1,25 @@
+
+const suma= (a,b) => a + b
+
+console.log( suma(12,10))
+console.log( suma(13,1))
+console.log( suma(2,102))
+
+
+/* 
+
+//Funcion tradicional
+function suma(a,b){ 
+    return a + b 
+} 
+
+
+//Funcion anonima FatArrow
+
+const suma= (a,b) =>{ 
+    return a + b 
+} 
+
+//Funcion anonima FatArrow simplificada
+
+const suma= (a,b) => a + b  */
