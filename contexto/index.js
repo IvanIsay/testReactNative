f80390c0d519e68904f07fddcea165dcf09be763
lOkY <102552151+IvanIsay@users.noreply.github.com>
2024-04-25 21:51:28 -0600

    Objetos y DOM

--- contexto/index.js
+++ contexto/index.js
@@ -1,12 +1,51 @@
 
-const suma= (a,b) => a + b
+const titulo= document.createElement('h1')
+titulo.innerText='Soy titulo desde JS'
+document.body.append(titulo)
 
-console.log( suma(12,10))
-console.log( suma(13,1))
-console.log( suma(2,102))
+
+const boton= document.createElement('button')
+boton.innerText='Presioname'
+document.body.append(boton)
+
+boton.addEventListener('click',function (){
+    //console.log('evento click ejecutado')
+    titulo.innerText= "Evento click Ejecutado"
+    alert('Exito') 
+})
 
 
 /* 
+const usuario={
+    nombre:'ana',
+    apellido: 'lopez',
+    edad: 30,
+    direccion:{
+        pais:'Mexico',
+        cuidad:'Qro',
+        calle: 'Las misma 123'
+    },
+    amigos:['juan','bety'],
+    estatus: true,
+    enviaCorreo: enviarCorreo = () => 'enviando ...' ,
+
+}
+
+//Propiedades
+//console.log(usuario)
+//console.log(usuario.nombre)
+//console.log(usuario.direccion)
+//console.log(usuario.direccion.calle)
+//console.log(usuario.amigos)
+//console.log(usuario.estatus)
+
+//Metodos
+console.log(usuario.enviaCorreo())
+
+
+
+
+
 
 //Funcion tradicional
 function suma(a,b){ 
@@ -22,4 +61,10 @@ const suma= (a,b) =>{
 
 //Funcion anonima FatArrow simplificada
 
-const suma= (a,b) => a + b  */
+const suma= (a,b) => a + b  
+
+console.log( suma(12,10))
+console.log( suma(13,1))
+console.log( suma(2,102))
+
+*/
