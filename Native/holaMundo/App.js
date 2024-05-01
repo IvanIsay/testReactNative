181ebc33bc396d59cdd82475c6602e453b307c26
lOkY <102552151+IvanIsay@users.noreply.github.com>
2024-04-30 23:44:17 -0600

    Componentes, Props, State

--- Native/holaMundo/App.js
+++ Native/holaMundo/App.js
@@ -1,10 +1,19 @@
+import React,{useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+const Texto= ()=>{
+  const [contenido, setContenido]= useState('Hola ReactNative :)')
+  const actualizaTexto= ()=>{ setContenido('State actulizado con exito') }
+  return(
+    <Text style={{fontSize:24}} onPress={actualizaTexto}> {contenido} </Text>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola Mundo React Native!! :) </Text>
+        <Texto/> 
       <StatusBar style="auto" />
     </View>
   );
