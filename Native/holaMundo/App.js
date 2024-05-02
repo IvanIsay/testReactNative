b05214be426947005450ca2369f2856267b73748
lOkY <102552151+IvanIsay@users.noreply.github.com>
2024-05-01 22:36:52 -0600

    StyleSheets, Flex

--- Native/holaMundo/App.js
+++ Native/holaMundo/App.js
@@ -2,29 +2,50 @@ import React,{useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-const Texto= ()=>{
-  const [contenido, setContenido]= useState('Hola ReactNative :)')
-  const actualizaTexto= ()=>{ setContenido('State actulizado con exito') }
+const Texto= ({style})=>{
+  const [contenido, setContenido]= useState('Hola Native :)')
+  const actualizaTexto= ()=>{ setContenido('actualizado ') }
   return(
-    <Text style={{fontSize:24}} onPress={actualizaTexto}> {contenido} </Text>
+    <Text style={[styles.text,style]} onPress={actualizaTexto}> {contenido} </Text>
   )
 }
 
 export default function App() {
   return (
     <View style={styles.container}>
-        <Texto/> 
+        <Texto style={styles.red}/>
+        <Texto style={styles.green}/> 
+        <Texto style={styles.blue}/>  
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  text:{
+    color:'white',
+    fontSize:28,
+
+  },
+  red:{
+    
+    backgroundColor:'red',
+  },
+  green:{
+    
+    backgroundColor:'green',
+  },
+  blue:{
+    
+    backgroundColor:'blue',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    color: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection:'column',
+    backgroundColor:'#fff',
+    alignItems: 'baseline',
+    justifyContent: 'space-evenly',
   },
+
+
 });
