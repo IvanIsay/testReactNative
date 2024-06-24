b3a9a3b0d76b992ca745da4c2b2c6506c66ffbcc
lOkY <102552151+IvanIsay@users.noreply.github.com>
2024-06-24 17:04:55 -0600

    sectionList

--- Native/helloW2/App.js
+++ Native/helloW2/App.js
@@ -1,99 +1,37 @@
 import React,{useState} from 'react';
-import { StyleSheet, Text, View,TextInput, Button,TouchableHighlight, ScrollView, Dimensions } from 'react-native';
-
-
+import { StyleSheet, Text, View, FlatList, SectionList} from 'react-native';
 
 export default function App() {
-
-  const[text, setText]= useState('default')
-  const[submit, setSubmit]= useState('')
-
   return (
     <View style={styles.container}>
 
-
-      <ScrollView style={styles.scrollView}>
-
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-        <Text> Componente TextInput: {submit} </Text>
-
-
-        <TextInput style={styles.input} 
-        placeholder='soy un input' 
-        onChangeText={t=> setText(t)} 
-        defaultValue={text} />
-
-        <Button onPress={ ()=>{
-          setSubmit(text)
-          alert('texto enviado correctamente')
-        } } title='Presioname '/> 
-
-        <TouchableHighlight
-           underlayColor={'#FFC300'}
-           activeOpacity={0.2}
-           onPress={ ()=>{
-            setSubmit(text)
-            alert('texto enviado correctamente')
-          } }>
-           <Text> Otro boton </Text> 
-        </TouchableHighlight>
-
-
-      </ScrollView>
+      <SectionList
+        sections={[
+          {title:'Grupo A',
+           data:[
+            {key:'1',name:'Ivan Isay'}, 
+            {key:'2',name:'Naomi Guerra'}, 
+            {key:'3',name:'Reyna Martinez'}, 
+            {key:'4',name:'Ana Lopez'}, ]
+          },
+          {title:'Grupo B',
+            data:[
+             {key:'5',name:'Ivan Isay'}, 
+             {key:'6',name:'Naomi Guerra'}, 
+             {key:'7',name:'Reyna Martinez'}, 
+             {key:'8',name:'Ana Lopez'}, ]
+           },
+           {title:'Grupo C',
+            data:[
+             {key:'9',name:'Ivan Isay'}, 
+             {key:'10',name:'Naomi Guerra'}, 
+             {key:'11',name:'Reyna Martinez'}, 
+             {key:'12',name:'Ana Lopez'}, ]
+           },
+        ]}
+        renderItem={  ({item})=> <Text style={styles.item}> {item.name}  </Text>  }
+        renderSectionHeader={ ({section})=> <Text style={styles.section} > {section.title}  </Text>}
+      />
 
     </View>
   );
@@ -104,19 +42,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor:'#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
+    paddingTop:40,
   },
-
-  input:{
-    height:40,
-    borderBottomColor: '#ccc',
+  item:{
+    padding:10,
+    fontSize:24,
+    height:50,
+    borderColor:'blue',
     borderBottomWidth:1,
-    width:'100%'
   },
-
-  scrollView:{
-    width:Dimensions.get('window').width
+  section:{
+    fontSize:16,
+    fontWeight:'bold',
+    backgroundColor:'#eee',
+    paddingTop:4,
+    paddingLeft:10,
+    paddingRight:10,
+    paddingBottom:4,
   }
 
 });
